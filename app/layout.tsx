bd6575abc84b1ac841d5b3c74a730c5885179eec
IsteakShupto <isteakshupto006@gmail.com>
2025-07-12 23:12:00 +0600

    added signalR and leaflet integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Navbar from "@/components/navbar";
 import QueryClientProviders from "@/components/providers";
 
+import "leaflet/dist/leaflet.css";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
